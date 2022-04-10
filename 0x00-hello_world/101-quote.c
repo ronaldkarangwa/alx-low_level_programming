9ef38404d5ff21b768eb1110469f8f1d7731d982
karangwa <gahima.ronald@yahoo.com>
2022-04-10 05:25:06 -0700

    ajsj

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,12 @@
 #include <unistd.h>
 
 /**
- *main - Entry point
+ * main - Entry Point
  *
- *Return: 1 
+ * Returh: 0 (Success)
  */
 int main(void)
 {
 write(1, "and that piece of art is useful\n" - Dora Korpar, 2015-10-19\n", 59);
-return (1)
+return (1);
 }
