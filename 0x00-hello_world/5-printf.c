243413bb58572a7c657158cc74066c769caa92af
karangwa <gahima.ronald@yahoo.com>
2022-04-10 04:08:10 -0700

    qrqtqy

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- *main - entry point
+ *main - Entry point
  *Hello, printf
- *Return: return 1 after program ends
+ *Return: 0 when the program ends
  */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
-return 0;
+return (0);
 }
