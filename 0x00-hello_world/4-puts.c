260bf81542aad86be0b5d1f80d9e2ca0aa796805
karangwa <gahima.ronald@yahoo.com>
2022-04-10 02:31:19 -0700

    4th

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,6 @@
-nclude <stdio.h>                                                                                                                                                                           
-
-int main(void)                                                                                                                                                                               
-
-{                                                                                                                                                                                            
-
-	puts("\"Programming is like building a multilingual puzzle");                                                                                                                                
-
-	return (0);                                                                                                                                                                                  
-
-
+#include <stdio.h>
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
