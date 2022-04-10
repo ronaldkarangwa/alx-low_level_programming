30c292ecff7b2204e3c7b9628185d4ebad89e233
karangwa <gahima.ronald@yahoo.com>
2022-04-10 02:19:34 -0700

    jdjdj

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+nclude <stdio.h>                                                                                                                                                                           
+
+int main(void)                                                                                                                                                                               
+
+{                                                                                                                                                                                            
+
+	puts("\"Programming is like building a multilingual puzzle");                                                                                                                                
+
+	return (0);                                                                                                                                                                                  
+
+
