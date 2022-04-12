27b815e2570ce3bcc821b835d4826a4a45b2a499
karangwa <gahima.ronald@yahoo.com>
2022-04-12 12:44:01 -0700

    qwqeqyqio

---
+++ 0x03-debugging/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - causees an infinite loop
+ *
+ * Return: 0
+ */
+
+int main(void)
+
+{
+int i;
+
+printf("Infinite loop incoming :(\n");
+
+i = 0;
+
+printf("infinite loop avoided! \\o/\n");
+
+return (0)
+
+}
