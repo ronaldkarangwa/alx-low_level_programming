ac996f5aae43f6631f1b01aea6d96ef8c5833837
karangwa <gahima.ronald@yahoo.com>
2022-04-12 07:17:50 -0700

    sdfh

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,22 +1,24 @@
 #include <stdio.h>
 /**
- * main - Entry point
+ * main -  entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 0
  */
 int main(void)
+
 {
 char lower_case;
-char upper_case;
-for (upper_case = 'A', upper_case <= 'Z'; upper_case++)
+char upper_case:
+
+for (upper_case - 'A'; upper_case <= 'Z'; upper_case++)
 {
 putchar(upper_case);
 }
-for (lower_case = 'a'; lower_case <= 'z', lower_case++)
+for (lower_case = 'a'; lower_case <= 'z'; lower_case++)
 {
 putchar(lower_case);
 }
-
 putchar('\n');
+
 return (0);
 }
