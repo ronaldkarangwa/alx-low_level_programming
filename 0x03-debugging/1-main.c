5113b942ad3178d6337c51c83438c68d653da12b
karangwa <gahima.ronald@yahoo.com>
2022-04-12 13:38:52 -0700

    dsfsjsk

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - infinite loop
+ *
+ * Return: Always 0
+ */
+int main(void)
+
+{
+int i;
+
+printf("Infinite loop incoming :(\ n");
+
+i = 0;
+
+/**
+ * while (i < 10)
+ * {
+ * Infinite loop - No increase of variable
+ * putchar(i);
+}
+*/
+printf("Infinite loop avoided! \\o/\n");
+
+return (0);
+}
+
