59311689e8490384150f81f31bd0eaeaee20a187
karangwa <gahima.ronald@yahoo.com>
2022-04-12 06:27:44 -0700

    dagah

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,14 +6,14 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{
-	char ermias[] = "_putchar";
-	int c;
-	for (c = 0; c < 8; c++)
-	{
-		_putchar(ermias[c]);
-	}
-	_putchar('\n');
-	return (0);
 
+{
+char ermias[] = "_putchar";
+int c;
+for (c = 0; c < 8; c++)
+{
+_putchar(ermias[c]);
+}
+_putchar('\n');
+return (0);
 }
