a370cea98754210a8951b69cad7d1fa00f671aa0
karangwa <gahima.ronald@yahoo.com>
2022-04-12 17:26:12 -0700

    dfauygihbdshsibuifsei

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,23 +1,17 @@
 #include "main.h"
 
-
-
 /**
- *
- *  * main - check the code.
- *
- *   *
- *
- *    * Return: Always 0.
- *
- *     */
-
-int main(void)
-
+ * print_alphabet_x10 - Print 10 times the alphabet, in lowercase.
+ */
+void print_alphabet_x10(void)
 {
- 
-print_alphabet_x10();
+int count = 0;
+char letter;
 
-return (0);
-
+while (count++ <= 9)
+{
+for (letter = 'a'; letter <= 'z'; letter++)
+_putchar(letter);
+_putchar('\n');
+}
 }
