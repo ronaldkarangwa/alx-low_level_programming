e9138bfafc9f3c9cfbdd8b1838b7463e9e29abac
karangwa <gahima.ronald@yahoo.com>
2022-04-13 16:57:30 -0700

    6th task

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _abs - function that creates the abs value of an integer
+ *
+ * @x: the number to check
+ *
+ * Return: int
+ */
+int _abs(int x)
+
+{
+
+if (x < 0)
+
+{
+
+x = x * (-1);
+
+return (x);
+
+}
+
+else
+
+return (x);
+
+}
