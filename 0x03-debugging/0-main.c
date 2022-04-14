6f2b334afa3efd4e46c9440c7e4d552c4dec7bd5
karangwa <gahima.ronald@yahoo.com>
2022-04-14 05:24:37 -0700

    jhhgdgh

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,10 @@
-#include "main.h"
-/**
- * main - check the code
- *
- * Return: Always 0
- */
-int main(void)
+#include <stdio.h>
 
 {
-int i;
-i = 0;
+	int i;
 
-positive_or_negative(i);
+	i = 98;
+	positive_or_negative(i);
 
-return (0);
+	return (0);
 }
