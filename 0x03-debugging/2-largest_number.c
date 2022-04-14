db8f2264b2e7167d973b642fd7650f266a9eb826
karangwa <gahima.ronald@yahoo.com>
2022-04-14 08:08:25 -0700

    shgshgdb

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - prints the largest of 3 integers
+ * Return: 0;
+ */
+int largest_number(int a, int b, int c)
+
+{
+int largest;
+
+if (a > b && a > c)
+{
+largest = a;
+}
+else if (b > a && b > c)
+{
+largest = b;
+}
