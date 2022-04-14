8a7806a97bd4384117a329cb704d12083bacb6a3
karangwa <gahima.ronald@yahoo.com>
2022-04-13 20:44:36 -0700

    swadafgw

---
+++ 0x01-variables_if_else_while/9-main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * main - checck code
+ *
+ * Return: 0
+ */
+int main(void)
+{
+times_table();
+return (0);
+}
---
+++ 0x01-variables_if_else_while/9-times_table.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - entry point
+ * Return: 0
+ */
+void times_table(void)
+
+{
+if (mult < 10)
+{
+if (col != 9)
+{
+_putchar(' ');
+_putchar(' ');
+}
+}
+if (col != 9)
+_putchar(',');
+_putchar('\n');
+}
