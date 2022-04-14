e807f37901867e882ab8421854aa99dee4de8923
karangwa <gahima.ronald@yahoo.com>
2022-04-13 18:10:24 -0700

    10th task

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * add - adds two interger and returns resullt
+ * @n1: interger 1
+ * @n2: interger 2
+ *
+ * Return:int
+ */
+
+int add(int n1, int n2)
+
+{
+
+int res;
+
+res = n1 + n2;
+
+return (res);
+
+}
---
+++ 0x02-functions_nested_loops/10-main.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+
+
+/**
+ * main check code
+ *
+ * Return: Always
+ */
+
+int main(void)
+
+{
+
+int n;
+
+n = add(89, 9);
+
+printf("%d\n", n);
+
+return (0);
+
+}
