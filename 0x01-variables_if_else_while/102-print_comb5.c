1bf404e948314b78050841b8c5f780e28b04c856
karangwa <gahima.ronald@yahoo.com>
2022-04-13 19:18:29 -0700

    12th task

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -17,46 +17,24 @@ int b;
 
 int com = 44;
 
-
 while (a <= 99)
-
 {
-
 b = a + 1;
-
 while (b <= 99)
-
 {
-
 putchar((a / 10) + n1);
-
 putchar((a % 10) + n1);
-
 putchar(32);
-
 putchar((b / 10) + n1);
-
 putchar((b % 10) + n1);
-
 if (a != 98 || b != 99)
-
 {
-
 putchar(com);
-
-putchar(32);
-
-							}
+putchar(32);							}
 b += 1;
-
 }
-
 a += 1;
-
 }
-
 putchar('\n');
-
 return (0);
-
 }
